04b253e7b967b7cda348c7b523c525d478523203
Jan Biasi <biasijan@gmail.com>
2018-06-22 00:14:31 +0200

    fix styles for mobile using flexbox

--- src/patterns/atoms/image/index.tsx
+++ src/patterns/atoms/image/index.tsx
@@ -14,14 +14,15 @@ export interface IImageBlock {
 	description?: string;
 }
 
-const RENDITION_BASE_SIZE = 50;
+const RENDITION_BASE_SIZE = 35;
 
 const calculateRenditions = (rendition: ImageRendition): number[] => {
-	return rendition
-		.split(':')
-		.map((size: string) => parseInt(size, 10))
-		.map((size: number) => (size >= 9 ? size / 2 : size))
-		.map((size: number) => size * RENDITION_BASE_SIZE);
+	const renditions = rendition
+		.split(':') // 1:1 => ['1', '1']
+		.map((size: string) => parseInt(size, 10)) // ['1', '1'] => [1, 1]
+		.map((size: number) => size * RENDITION_BASE_SIZE); // [1, 1] => [35, 35]
+
+	return renditions.map((size: number) => size * (100 / (renditions[0] + renditions[1])) * 5);
 };
 
 export const Image = ({ rendition, modifier }: IImage) => {
@@ -31,6 +32,7 @@ export const Image = ({ rendition, modifier }: IImage) => {
 		<div className={`a-image` + (modifier ? ` a-image--${modifier}` : '')} style={{ width, height }}>
 			<p className="a-image__rendition" style={{ lineHeight: `${height}px` }}>
 				{rendition}
+				{modifier && ` (${modifier})`}
 			</p>
 		</div>
 	);
